a88cea9e4c63cc4fd0a0e62b3137b27d8872f94a
ELITEDARKCODER <151459971+11Yazan11@users.noreply.github.com>
2024-12-18 15:21:25 +0100

    Update player.py

--- public/player.py
+++ public/player.py
@@ -1,1 +1,24 @@
+import pygame
+import socketio
+import sys
+import math
+import ctypes
+
+class Player:
+    def __init__(self, game, x, y, w, h, color, name, angle, bullets, kills, id):
+        self.game = game
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.color = color
+        self.name = name
+        self.angle = angle
+        self.bullets = bullets
+        self.kills = kills
+        self.id = id
+        self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
+    def draw(self):
+        pygame.draw.rect(self.game.screen, self.color, self.rect)
+        # Handle name, angle, bullets, kills, (and id) later.
 
